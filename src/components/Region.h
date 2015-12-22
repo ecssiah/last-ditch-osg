cc1c073e1eccf96e06b7d9d83d17d1fac03c473d
kabbotta <08kabbotta80@gmail.com>
2015-12-22 01:46:03 -0600

    added door generation to map system

---
+++ src/components/Region.h
@@ -0,0 +1,24 @@
+#ifndef REGION_H
+#define REGION_H
+
+#include <osg/Vec3>
+
+namespace ld
+{
+
+struct Region
+{
+  Region(int x_, int y_, int w_, int h_)
+    : x(x_),
+      y(y_),
+      w(w_),
+      h(h_)
+  {}
+
+  int x, y;
+  int w, y;
+};
+
+}
+
+#endif /* REGION_H */
